00f88180cce70e8dd48233b99e72ae941b5c6c89
Arnaud Wald <arnaudwald@gmail.com>
2016-05-30 09:45:58 +0200

    Working part I with gurobi solver

--- quadcopter/main.m
+++ quadcopter/main.m
@@ -7,57 +7,58 @@ load('quadData.mat')
 outerController = getOuterController(Ac, 'cplex');
 disp('Data successfully loaded')
 
-
-
 %% %%%%%%%%%%%%%% First MPC controller %%%%%%%%%%%%%%%%%%%
 
-N = 10;
-T = 2;
+% Horizon
+N = 20;
 
-x0 = [-1 10*pi/180 -10*pi/180 120*pi/180 0 0 0]';
-
-Q = eye(7);
+% Parameters for cost function 
+Q = diag([50 1e4 1e4 2 1e-1 1e-1 1e-1]);
 R = eye(4);
 
+% Constraints
 zpMax = 1;                  % [m/s]
 alphaMax = 10*pi/180;       % [rad]
-angleMax = [alphaMax; alphadMax];
+angleMax = [alphaMax; alphaMax];
 alphadMax = 15*pi/180;      % [rad/s]
 angledMax = [alphadMax; alphadMax];
 gammadMax = 60*pi/180;      % [rad/s]
 
-x = sdpvar(7,N+1,'full');
-u = sdpvar(4,N,'full');
+% Steady-state input constraints
+uMin = -us;
+uMax = ones(4,1)-us;
 
+% Terminal constraint using dlqr
 [~,S,~] = dlqr(sys.A, sys.B, Q, R, zeros(7, 4));
 
-constraints = [];
-for i = 1:N
+% System definition
+x = sdpvar(7,N,'full'); u = sdpvar(4,N,'full');
+constraints = []; objective = 0;
+for i = 1:N-1
+    % System dynamics
     constraints = constraints + (x(:,i+1) == sys.A*x(:,i) + sys.B*u(:,i));
-end
-constraints = constraints + (x(:,1) == x0);
-
-for i = 1:N
+   
+    % State constraints
     constraints = constraints + (-zpMax <= x(1,i+1) <= zpMax);
     constraints = constraints + (-angleMax <= x(2:3,i+1) <= angleMax);
-    constraints = constraints + (-angledMax <= x(4:5,i+1) <= angledMax);
-    constraints = constraints + (-gammadMax <= x(6,i+1) <= gammadMax);
+    constraints = constraints + (-angledMax <= x(5:6,i+1) <= angledMax);
+    constraints = constraints + (-gammadMax <= x(7,i+1) <= gammadMax);
+   
+    % Input Constraints
+    constraints = constraints + (uMin <= u(:,i) <= uMax);
     
-    constraints = constraints + (0 <= u(:,i) <= 1);
-end
-
-objective = 0;
-for i = 1:N
+    % Cost function
     objective = objective +  x(:,i)'*Q*x(:,i) + u(:,i)'*R*u(:,i);
-end 
-objective = objective + x(:,N+1)'*S*x(:,N+1);
+end
+objective = objective + x(:,N)'*S*x(:,N);
 
-options = sdpsettings('solver','qpip');
+% Simulate the model
+T = 10;% Time horizon
+x0 = [-1 10*pi/180 -10*pi/180 120*pi/180 0 0 0]'; % Initial state
+
+options = sdpsettings('solver','gurobi');
 innerController = optimizer(constraints, objective, options, x(:,1), u(:,1));
 simQuad(sys, innerController, x0, T);
-
-%%
-pause
 
 %% Reference tracking - no disturbance, no invariant sets
 fprintf('PART II - reference tracking...\n')
